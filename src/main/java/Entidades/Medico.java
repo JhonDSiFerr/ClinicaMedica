eb466ec85c8c5ea00e3375e9a5eacba01c4b7488
Usuario <Usuario@DESKTOP-0495GTB>
2025-06-23 20:31:36 -0300

    Ja tem os button modificados

---
+++ src/main/java/Entidades/Medico.java
@@ -0,0 +1,88 @@
+package Entidades;
+
+public class Medico {
+    private String Nome;
+    private String Cpf;
+    private String Endereco;
+    private String Telefone;
+    private String Crm;
+    private String EstadoCivil;
+    private String DataNascimento;
+    private Especialidade especialidade;
+    private String Observacoes ;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String cpf) {
+        Cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        Endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getCrm() {
+        return Crm;
+    }
+
+    public void setCrm(String crm) {
+        Crm = crm;
+    }
+
+    public String getEstadoCivil() {
+        return EstadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        EstadoCivil = estadoCivil;
+    }
+
+    public String getDataNascimento() {
+        return DataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        DataNascimento = dataNascimento;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getObservacoes() {
+        return Observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        Observacoes = observacoes;
+    }
+
+    }
+
+
---
+++ src/main/java/Entidades/consulta.java
@@ -0,0 +1,9 @@
+package Entidades;
+
+public class consulta {
+    private String Data;
+    private Paciente paciente;
+    private Especialidade especialidade;
+    private Medico medico;
+    private char convenio;
+}
